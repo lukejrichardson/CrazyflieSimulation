5c98de969a97436c75a6a8f4f9c868b90e177519
Luke Richardson <rchluk003@myuct.ac.za>
2024-03-15 09:39:58 +0200

    Initial commit

---
+++ supporting/initVars.m
@@ -0,0 +1,26 @@
+g = 9.81;
+
+m = 0.027;
+
+% Body inertia matrix
+ I = 10^(-6)*[16.57   0.83   0.72;...
+            0.83    16.66   1.8;...  
+            0.72    1.8     29.26];                % kg.m^2
+
+%I = 10^(-6)*[16.57   0      0;...
+%            0       16.66   1.8;...  
+%            0       1.8     29.26];                % kg.m^2
+
+I_inv = inv(I);
+
+% Thrust coefficient
+k_t = (3.1582*10^(-10))/(((2*pi())/60)^2);     % N/(rad/s)
+
+% Prop drag coefficient
+k_q = (7.9379*10^(-12))/(((2*pi())/60)^2);    % N.m/(rad/s)
+
+% Motor rotor/prop inertia
+J_m = 1*10^(-6);    % kg.m^2
+
+% Adjacent motor distance
+d = 0.06761;    % m
