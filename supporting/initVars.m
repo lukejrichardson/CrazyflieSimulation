a499151de4147dc1db8d66cce56066a5fb78d8a1
lukejrichardson <rchluk003@myuct.ac.za>
2024-06-18 12:19:27 +0200

    gut commut

--- supporting/initVars.m
+++ supporting/initVars.m
@@ -8,10 +8,10 @@ Ts_PID_position = 0.01; % s
 Ts_PID_attitude = 0.002; % s
 
 % Accurate body inertia matrix [kg.m^2]
-% I = 10^(-6)*[16.57   0.83   0.72; 0.83    16.66    1.8; 0.72    1.8     29.26];
+I = 10^(-6)*[16.57   0.83   0.72; 0.83    16.66    1.8; 0.72    1.8     29.26];
 
 % Diagonal body inertia matrix [kg.m^2]
-I = 10^(-6)*[16.57   0   0; 0    16.66   0; 0    0     29.26];
+% I = 10^(-6)*[16.57   0   0; 0    16.66   0; 0    0     29.26];
 
 % Alternate body inertia matrix [kg.m^2]
 % I = 10^(-6)*[16.57*1.2      0.83        0.72/10;...
